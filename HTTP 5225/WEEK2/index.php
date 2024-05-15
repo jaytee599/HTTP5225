80824f3ed5e27fddf2b1cd5c72fd2727ad97192f
JOEL KOFI MIREKU TUFFOUR <156682787+jaytee599@users.noreply.github.com>
2024-05-15 17:55:59 -0400

    Add files via upload

---
+++ HTTP 5225/WEEK2/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>week 2</title>
+</head>
+<body>
+
+<?php echo 'Hello'; ?>
+<?php echo '<h1>Welcome to  week 2</h1>'; ?>
+<?php echo '<p>PHP\'s is fun</p>'; ?>
+
+<h1> Welcome to HTTP 5225</h1>
+<p>we will learn Php in this class</p>
+<p>Things we will learn in the class</p>
+
+<?php echo
+'<ul> 
+    <li>PHP</li>
+    <li>mySQL</li>
+    <li>Laravel</li> 
+</ul>';
+?>
+
+<?php echo '<img src="https://www.1zoom.me/prev/303/302250.jpg" alt="Placeholder image" width="500" height="250">'; ?>   
+
+ <?php
+ $fname = 'Joel';
+ $lname = 'Tuffour';
+ $name['fname'] = 'Joel';
+//$name = array ['Joel' . 'Tuffour'];
+?>
+
+<?php 
+echo '<br>';
+echo 'Hello ' . $fname . ' ' . $lname;
+?>
+
+</body>
+</html>
